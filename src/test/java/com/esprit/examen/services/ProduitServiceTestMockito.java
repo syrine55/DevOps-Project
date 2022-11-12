00998e65a17d4b98b1182e739641529aba3e79cf
syrine55 <syrine.timoumi@esprit.tn>
2022-11-12 23:58:38 +0100

    crud test 4

--- src/test/java/com/esprit/examen/services/ProduitServiceTestMockito.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceTestMockito.java
@@ -1,21 +1,24 @@
 package com.esprit.examen.services;
-
+import com.esprit.examen.entities.Facture;
 import com.esprit.examen.entities.Produit;
-import com.esprit.examen.entities.Stock;
+import com.esprit.examen.repositories.FactureRepository;
 import com.esprit.examen.repositories.ProduitRepository;
-import com.esprit.examen.repositories.StockRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-
+import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+@SpringBootTest
 
 public class ProduitServiceTestMockito {
     @Mock
