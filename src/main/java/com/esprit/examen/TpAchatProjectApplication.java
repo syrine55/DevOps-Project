e63f94503e1898bd141889861417d1ced47185e6
syrine55 <syrine.timoumi@esprit.tn>
2022-11-13 14:54:29 +0100

    crud test 4

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -8,9 +8,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class TpAchatProjectApplication {
+public class TpAchatProjectApplication {/*
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpAchatProjectApplication.class, args);
-}
+}*/
 }
--- src/main/java/com/esprit/examen/entities/Stock.java
+++ src/main/java/com/esprit/examen/entities/Stock.java
@@ -30,11 +30,6 @@ public class Stock implements Serializable {
 	@OneToMany(mappedBy = "stock")
 	@JsonIgnore
 	private Set<Produit> produits;
-	public Stock(String libelleStock, Integer qte, Integer qteMin) {
-		super();
-		this.libelleStock = libelleStock;
-		this.qte = qte;
-		this.qteMin = qteMin;
-	}
+
 
 }
